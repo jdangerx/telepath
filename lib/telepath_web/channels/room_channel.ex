6818be1dacacbedd80d26f5f9d90ca0eb8712865
Dylan Sherry <sharshofski@gmail.com>
2018-11-29 14:22:54 -0500

    Whitespace

--- lib/telepath_web/channels/room_channel.ex
+++ lib/telepath_web/channels/room_channel.ex
@@ -11,5 +11,5 @@ defmodule TelepathWeb.RoomChannel do
   def handle_in("new_msg", %{"body" => body}, socket) do
     broadcast!(socket, "new_msg", %{"body" => body})
     {:noreply, socket}
-  end 
+  end
 end

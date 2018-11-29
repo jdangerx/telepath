eea45c68fb8a21c780b6a771e1132b155b08bb85
John Xia <john.danger.xia@gmail.com>
2018-11-29 13:00:21 -0500

    We have this terrible chat app kind of working!

--- lib/telepath_web/channels/user_socket.ex
+++ lib/telepath_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule TelepathWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", TelepathWeb.RoomChannel
+  channel "room:*", TelepathWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
